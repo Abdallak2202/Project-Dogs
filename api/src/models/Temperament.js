db92e10da733440ac695402b4c9bd225a92428a9
Abdallak2202 <96998579+Abdallak2202@users.noreply.github.com>
2022-12-06 16:41:50 -0500

    database models created

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -3,27 +3,14 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('temperament', {
     id: {
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true
+      defaultValue: DataTypes.UUIDV4
     },
     name: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    height: {
-      type: DataTypes.NUMBER,
-      allowNull: false
-    },
-    weight: {
-      type: DataTypes.NUMBER,
-      allowNull: false
-    },
-    lifespan: {
-      type: DataTypes.NUMBER
     }
   });
 };
